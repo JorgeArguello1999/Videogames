b3bbd98fbcee9edd15b3514f76f918adf7185785
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2024-03-08 13:20:51 -0500

    feat: Filtro creado

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 # Importamos los datos 
 from data.csv_read import read_data_csv
 
-data = read_data_csv('./data/games.csv')
-print(data)
+def raiting(best_or_worst:bool=True, items:int=10):
+    """
+    best_or_worst -> True para ver los mejores, False para ver los peores
+    items -> Número de items que quiere obtener
+    """
 
+    # Datos del json 
+    data = read_data_csv('./data/games.csv')
+
+    # Ordenamos por Ranking
+    data.sort(key=lambda x: float(x['Rating']) if x['Rating'].strip() else 0, reverse=best_or_worst)
+
+    # Obtenemos el número de registros
+    if items <= 0: items = 1
+    if items > len(data): items = len(data) 
+
+    return [data[i] for i in range(items)]
 
 if __name__ == '__main__':
-    pass
+    dates = raiting(items=2, best_or_worst=False)
+    print(dates)
